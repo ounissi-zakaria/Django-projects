9752a390ebf89bbfa70ff1116980df533d32f4c7
zaki98D <ounissizakaria@gmail.com>
2021-07-25 11:50:05 +0100

    use postgresql instead of sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': "postgres",
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
