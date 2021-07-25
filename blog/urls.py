b3ccdd0b54f46bf5caa8a4f1b036ebd743f0ca10
zaki98D <ounissizakaria@gmail.com>
2021-07-25 04:15:24 +0100

    test github actions.

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("", views.IndexView.as_view(), name="index"),path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:topic_id>/comment", views.comment, name="comment"),
 ]
