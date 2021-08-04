3e23bab9620855b7a76e9b9b8b8d1b8bd94532b9
zaki98D <ounissizakaria@gmail.com>
2021-08-04 18:15:14 +0100

    add blog index template and style and remove comment from topic admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,16 +2,11 @@ from django.contrib import admin
 from .models import Topic, Comment
 
 
-class CommentInline(admin.TabularInline):
-    model = Comment
-    extra = 3
-
 
 class TopicAdmin(admin.ModelAdmin):
     fields = ["topic_title", "topic_body", "topic_date"]
     list_display = ["topic_title", "topic_date", "number_of_comments"]
     search_fields = ["topic_title"]
-    inlines = [CommentInline]
 
 
 admin.site.register(Topic, TopicAdmin)
